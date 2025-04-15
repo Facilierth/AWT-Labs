a6996a0b687052a9676e3071af46c425aa127c65
Katarzyna Jaroszynska <katri.jaroszynska@gmail.com>
2025-04-15 23:49:41 +0200

    i need sleep

--- first-graphql-app/src/App.js
+++ first-graphql-app/src/App.js
@@ -6,24 +6,12 @@ import axios from 'axios'
 
 const typeDefs = readFileSync(join('src', 'schema.graphql'), 'utf-8')
 
-const usersList = [
-    { id: 1, name: "Jan Konieczny", email: "jan.konieczny@wonet.pl", login: "jkonieczny" },
-    { id: 2, name: "Anna Wesołowska", email: "anna.w@sad.gov.pl", login: "anna.wesolowska" },
-    { id: 3, name: "Piotr Waleczny", email: "piotr.waleczny@gp.pl", login: "p.waleczny" }
-];
-const todosList = [
-    { id: 1, title: "Naprawić samochód", completed: false, user_id: 3 },
-    { id: 2, title: "Posprzątać garaż", completed: true, user_id: 3 },
-    { id: 3, title: "Napisać e-mail", completed: false, user_id: 3 },
-    { id: 4, title: "Odebrać buty", completed: false, user_id: 2 },
-    { id: 5, title: "Wysłać paczkę", completed: true, user_id: 2 },
-    { id: 6, title: "Zamówic kuriera", completed: false, user_id: 3 },
-];
-function todoById(parent, args, context, info){
-    return todosList.find(t => t.id == args.id);
-}
-function userById(parent, args, context, info){
-    return usersList.find(u => u.id == args.id);
+const usersList = [];
+const todosList = [];
+
+async function userById(parent, args, context, info){
+    const users = await getRestUsersList();
+    return users.find(u => u.id == args.id);
 }
 
 async function getRestUsersList(){
@@ -39,23 +27,45 @@ async function getRestUsersList(){
     } catch (error) {
         throw error
     }
-} 
+}
+
+async function getRestTodosList(){
+    try {
+        // pls work
+        const todos = await axios.get("https://jsonplaceholder.typicode.com/todos");
+        return todos.data.map(({ id, title, completed, userId }) => ({
+            id,
+            title,
+            completed,
+            user_id: userId
+        }));
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function todoById(parent, args) {
+    const todos = await getRestTodosList();
+    return todos.find(t => t.id == args.id);
+}
 
 const resolvers = {
     Query: {
         users: async () => getRestUsersList(),
-        todos: () => todosList,
-        todo: (parent, args, context, info) => todoById(parent, args, context, info),
-        user: (parent, args, context, info) => userById(parent, args, context, info),
+        todos: async () => getRestTodosList(),
+        todo: (parent, args) => todoById(parent, args),
+        user: (parent, args) => userById(parent, args),
     },
-    User:{
-        todos: (parent, args, context, info) => {
-            return todosList.filter(t => t.user_id == parent.id);
+    User: {
+        todos: async (parent) => {
+            const todos = await getRestTodosList();
+            return todos.filter(t => t.user_id == parent.id);
         }
     },
-    ToDoItem:{
-        user: (parent, args, context, info) => {
-            return usersList.find(u => u.id == parent.user_id);
+    ToDoItem: {
+        user: async (parent) => {
+            const users = await getRestUsersList();
+            return users.find(u => u.id == parent.user_id);
         }
     }
 }
